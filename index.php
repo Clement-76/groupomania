298c4b81466b98dfa6104abe97d4187c81d34faf
Clement-76 <clement.patigny@gmail.com>
2019-02-15 00:27:38 +0100

    creation of the router and adding composer

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require 'composer/vendor/autoload.php';
+
+session_start();
+
+$action = 'login';
+$controller = '\ClementPatigny\Controller\\'; // the namespace
+
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    // the format of the GET parameter is controller.action
+    $data = explode('.', $_GET['action']);
+    $action = $data[1];
+    $controller .= ucfirst($data[0]) . 'Controller';
+} else {
+    $controller .= 'UsersController';
+}
+
+if (class_exists($controller)) {
+    $controller = new $controller();
+    $controller->$action();
+} else {
+    header("HTTP/1.0 404 Not Found");
+    exit;
+}

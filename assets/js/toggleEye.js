45742c65f11f11310e266c57f2050dd6d65f212e
Clement-76 <clement.patigny@gmail.com>
2019-02-19 03:48:40 +0100

    adding the toggleEye function to hide/show the password on forms

---
+++ assets/js/toggleEye.js
@@ -0,0 +1,19 @@
+/**
+ * toggle the source of an element (for example: img, a)
+ * between two sources
+ * @param object elt the target element of the toggle
+ * @param array src the sources to toggle
+ */
+function toggleEye(elt, src) {
+    if (elt.getAttribute('src') === src[0]) {
+        elt.src = src[1];
+        $(elt).siblings().attr("type", "password");
+    } else {
+        elt.src = src[0];
+        $(elt).siblings().attr("type", "text");
+    }
+}
+
+$(".eye").on("click", (e) => {
+    toggleEye(e.target, ['assets/images/visible_eye_icon.svg', 'assets/images/invisible_eye_icon.svg']);
+});
